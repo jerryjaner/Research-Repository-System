567f7ab0eb4a7153a8e3f92c8dbdc23013fd5ebe
Jerry Janer <janerjerry99@gmail.com>
2024-09-28 11:06:46 +0800

    Add FrontEnd

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\ResearchController;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\CompleteResearchController;
-use App\Http\Controllers\Admin\ResearchAbstractController;
 use App\Http\Controllers\HomePageController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 
 
 Route::get('/', [HomePageController::class, 'index'])->name('homepage');
@@ -39,6 +24,7 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         Route::get('/edit', [ResearchController::class, 'edit'])->name('edit');
         Route::post('/update', [ResearchController::class, 'update'])->name('update');
         Route::delete('/delete', [ResearchController::class, 'delete'])->name('delete');
+
     });
 
 });
